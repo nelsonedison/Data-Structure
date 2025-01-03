7d4b4b1ce394219fd525a3892495d6d9cf3a8993
Nelson joseph <167796677+nelsonedison@users.noreply.github.com>
2025-01-03 15:07:05 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter two numbers:/n")
+scanf("%d""%d",&a,&b);
+c=a+b;
+printf("sum of %d and %d is. %d,a,b,c")
+}
---
+++ swap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b,temp;
+print("enter two numbers:\n");
+scanf("%d %d",&a,&b);
+a=b;
+b=temp;
+a=temp;
+printf("values after swaping=\n");
+printf("a=%d\n",a);
+printf("b=%d\n",b);
+}
